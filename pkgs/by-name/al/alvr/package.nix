b80f4bbb72d4feef3c2b2a080e3ea33a5f20f4cc
Nylme <nylme@protonmail.com>
2024-09-11 21:51:34 +1000

    fixing alvr

--- pkgs/by-name/al/alvr/package.nix
+++ pkgs/by-name/al/alvr/package.nix
@@ -1,34 +1,89 @@
-{ lib,
-  appimageTools,
+{
+  stdenv,
+  fetchzip,
   fetchurl,
+  glib,
+  lib,
+  alsa-lib,
+  gtk3,
+  libunwind,
+  x264,
+  vulkan-loader,
+  xorg,
+  libva,
+  libdrm,
+  libvdpau,
+  libbsd,
+  libmd,
+  xz,
 }:
-let
+stdenv.mkDerivation rec {
   pname = "alvr";
-  version = "20.6.1";
-  src = fetchurl {
-    url = "https://github.com/alvr-org/ALVR/releases/download/v${version}/ALVR-x86_64.AppImage";
-    hash = "sha256-IYw3D18xUGWiFu74c4d8d4tohZztAD6mmZCYsDNxR+A=";
+  version = "v20.11.0";
+
+  src = fetchzip {
+    url = "https://github.com/alvr-org/ALVR/releases/download/${version}/alvr_streamer_linux.tar.gz";
+    name = "${pname}-${version}";
+    sha256 = "sha256-GfoXYzW3TUgpM5TXuHmsfhE9rP2M8iJpflF48tpmwIs=";
   };
 
-  appimageContents = appimageTools.extractType2 { inherit pname version src; };
-in
-appimageTools.wrapType2 {
-  inherit pname version src;
+  sourceRoot = ".";
 
-  extraInstallCommands = ''
-    install -Dm444 ${appimageContents}/alvr.desktop -t $out/share/applications
-    substituteInPlace $out/share/applications/alvr.desktop \
-      --replace-fail 'Exec=alvr_dashboard' 'Exec=alvr'
-    cp -r ${appimageContents}/usr/share/icons $out/share
+  rpath = lib.makeLibraryPath [
+    stdenv.cc.cc.lib
+    alsa-lib
+    gtk3
+    glib
+    x264.lib
+    vulkan-loader
+    xorg.libX11
+    xorg.libXau
+    xorg.libXdmcp
+    xorg.libXext
+    xorg.libXfixes
+    libva
+    libdrm
+    libunwind
+    libbsd
+    libmd
+    xz
+  ];
+
+  preferLocalBuild = true;
+  dontBuild = true;
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin $out/opt/ALVR $out/share/vulkan/explicit_layer.d $out/share/applications/ $out/share/icons/hicolor/256x256/apps/
+    mv ${src.name}/* $out/opt/ALVR
+
+    cp ${./alvr.png} $out/share/icons/hicolor/256x256/apps/alvr.png
+    cp ${./alvr.desktop} $out/share/applications/alvr.desktop
+
+    ln -s $out/opt/ALVR/bin/alvr_dashboard $out/bin 
+    ln -s $out/opt/ALVR/share/vulkan/explicit_layer.d/alvr_x86_64.json $out/share/vulkan/explicit_layer.d
+
+    runHook postInstall
+  '';
+  fixupPhase = ''
+    runHook preFixup
+
+    patchelf --set-rpath "${rpath}" --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/opt/ALVR/bin/alvr_dashboard
+    patchelf --set-rpath "${rpath}" --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/opt/ALVR/libexec/alvr/vrcompositor-wrapper
+    patchelf --set-rpath "${rpath}" $out/opt/ALVR/libexec/alvr/alvr_drm_lease_shim.so
+    patchelf --set-rpath "${rpath}" $out/opt/ALVR/lib64/libalvr_vulkan_layer.so
+    patchelf --set-rpath "${rpath}" $out/opt/ALVR/lib64/alvr/bin/linux64/driver_alvr_server.so
+
+    sed -i "s#../../../lib64/libalvr_vulkan_layer.so#$out/opt/ALVR/lib64/libalvr_vulkan_layer.so#" $out/opt/ALVR/share/vulkan/explicit_layer.d/alvr_x86_64.json
+
+    runHook postFixup
   '';
 
   meta = with lib; {
-    description = "Stream VR games from your PC to your headset via Wi-Fi";
-    homepage = "https://github.com/alvr-org/ALVR/";
-    changelog = "https://github.com/alvr-org/ALVR/releases/tag/v${version}";
+    description = "ALVR - Stream VR games from your PC to your headset via Wi-Fi.";
+    homepage = "https://github.com/alvr-org/ALVR";
+    maintainers = with maintainers; [ ];
     license = licenses.mit;
-    mainProgram = "alvr";
-    maintainers = with maintainers; [ passivelemon ];
     platforms = [ "x86_64-linux" ];
   };
 }
